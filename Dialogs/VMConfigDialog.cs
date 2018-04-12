5fffa0ffbab31dab9786c8efffe6fde07d0e0089
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-04-12 18:01:04 +0530

    Newer changes

--- Dialogs/VMConfigDialog.cs
+++ Dialogs/VMConfigDialog.cs
@@ -8,11 +8,19 @@ namespace Microsoft.Bot.Sample.ProactiveBot
     [Serializable]
     internal class VMConfigDialog : IDialog<object>
     {
+        /**
+         * Dictionary to store the various VM options
+         **/
+
         public IDictionary<string, string> VMOptions = new Dictionary<string, string>
         {
             {"1", "New VM creation" },
             {"2", "Adding storage to an existing VM " }
         };
+
+        /**
+         * StartAsync is the method implemented to mark the start of a IDialog
+         **/
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -28,20 +36,38 @@ namespace Microsoft.Bot.Sample.ProactiveBot
                 );  
         }
 
+        /**
+         * Resume method being carried out after the PromptDialog creation for different VM options
+         **/
+
         private async Task ResumeAfterChoiceClarification(IDialogContext context, IAwaitable<string> result)
         {
             var message = await result;
-            switch(message)
+
+            /**
+             * Switch case to handle different choice made by the user from the VMOptions Dialog
+             **/
+
+            switch (message)
             {
+                // Case for the VM creation and setup
+
                 case "New VM creation":
                     await context.PostAsync($"I would require some details to create a VM for you");
                     context.Call(new FormDialog(), ChildDialogComplte);
                 break;
+
+                // Case for adding extra storage to the existing VM
+
                 case "Adding storage to an existing VM":
                     context.Call(new StorageAdditionDialog(), ChildDialogComplte);
                 break;
             }
         }
+
+        /**
+         * Child dialog to handle the activity when the VMConfigDialog is closed
+         **/
 
         private async Task ChildDialogComplte(IDialogContext context, IAwaitable<object> result)
         {
