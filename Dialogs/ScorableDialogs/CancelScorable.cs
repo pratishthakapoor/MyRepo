56c033483a3d61c133c2cfa8d5e716c05cc2c2ea
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-04-20 17:09:35 +0530

    commit changes

---
+++ Dialogs/ScorableDialogs/CancelScorable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Builder.Internals.Fibers;
+using Microsoft.Bot.Builder.Scorables.Internals;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ProactiveBot.Dialogs.ScorableDialogs
+{
+    public class CancelScorable : ScorableBase<IActivity, string, double>
+    {
+        private readonly IDialogTask task;
+        
+        public CancelScorable(IDialogTask task)
+        {
+            SetField.NotNull(out this.task, nameof(task), task);
+        }
+
+        protected override Task DoneAsync(IActivity item, string state, CancellationToken token)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override double GetScore(IActivity item, string state)
+        {
+            return 1.0;
+        }
+
+        protected override bool HasScore(IActivity item, string state)
+        {
+            return state != null;
+        }
+
+        protected override async Task PostAsync(IActivity item, string state, CancellationToken token)
+        {
+            this.task.Reset();
+        }
+
+        protected override async Task<string> PrepareAsync(IActivity activity, CancellationToken token)
+        {
+            var message = activity as IMessageActivity;
+
+            if (message != null && !string.IsNullOrWhiteSpace(message.Text))
+            {
+                if (message.Text.Equals("cancel", StringComparison.InvariantCultureIgnoreCase) || message.Text.Equals("reset", StringComparison.InvariantCultureIgnoreCase) ||
+                    message.Text.Equals("restart", StringComparison.InvariantCultureIgnoreCase) || message.Text.Equals("start again", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return message.Text;
+                }
+            }
+            return null;
+        }
+    }
+}
