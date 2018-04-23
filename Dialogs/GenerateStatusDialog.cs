d5b24c6680a340d11d390666f11605ef83c783f7
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-04-23 16:23:41 +0530

    commit changes

--- Dialogs/GenerateStatusDialog.cs
+++ Dialogs/GenerateStatusDialog.cs
@@ -188,6 +188,10 @@ namespace Microsoft.Bot.Sample.ProacticeBot
                 Subtitle = status,
                 //Detail text
                 Text = "Latest work carried out on your raised ticket includes:\n\n" + notesresult,
+                //in case for other channel use
+                /**
+                 * Text = "Latest work carried out on your raised ticket includes:\n\n" + notesresult, ex : Text = "More words <br> New line <br> New line <b><font color = \"#11b92f\>GREEN</font></b></br></br>
+                 **/
                 //list of buttons
                 Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Need further details? ", value: "https://www.t-systems.hu/about-t-systems/customer-contact/service-desk"),
                     new CardAction(ActionTypes.OpenUrl, "Contact us at", value: "https://www.t-systems.com/de/en/contacts")}
