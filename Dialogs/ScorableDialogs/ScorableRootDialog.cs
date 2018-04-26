ef202204d6e0548c002a33c5c9040f5a0a40d7ff
Rohit Sinha <rohit.sinhalala@gmail.com>
2018-04-26 16:22:29 +0530

    commit changes

--- Dialogs/ScorableDialogs/ScorableRootDialog.cs
+++ Dialogs/ScorableDialogs/ScorableRootDialog.cs
@@ -15,11 +15,11 @@ namespace ProactiveBot.Dialogs.ScorableDialogs
 {
     public class ScorableRootDialog : ScorableBase<IActivity, string, double>
     {
-        private readonly IDialogStack stack;
+        private readonly IDialogTask task;
 
-        public ScorableRootDialog(IDialogStack stack)
+        public ScorableRootDialog(IDialogTask task)
         {
-            SetField.NotNull(out this.stack, nameof(stack), stack);
+            SetField.NotNull(out this.task, nameof(task), task);
         }
 
         protected override Task DoneAsync(IActivity item, string state, CancellationToken token)
@@ -29,21 +29,58 @@ namespace ProactiveBot.Dialogs.ScorableDialogs
 
         protected override double GetScore(IActivity item, string state)
         {
-            return state != null && state == "scorable1-triggered" ? 1 : 0;
+            return 1.0;
         }
 
         protected override bool HasScore(IActivity item, string state)
         {
-            return state != null && state == "scorable1-triggered";
+            return state != null;
         }
 
-        protected override Task PostAsync(IActivity item, string state, CancellationToken token)
+        protected override async Task PostAsync(IActivity item, string state, CancellationToken token)
         {
-            var message = item as IMessageActivity;
+            /*var message = item as IMessageActivity;
             var dialog = new RaiseDialog();
             var interruption = dialog.Void(stack);
             stack.Call(interruption, null);
-            return Task.CompletedTask;
+            return Task.CompletedTask;*/
+
+            var message = item as IMessageActivity;
+            IDialog<IMessageActivity> interruption = null;
+            if(message != null)
+            {
+                var incomingMessage = message.Text.ToLowerInvariant();
+                var messageToSend = String.Empty;
+
+                if(incomingMessage.Contains("how are you"))
+                {
+                    messageToSend = "I am good. How about you?";
+                }
+                else if(incomingMessage.Contains("i am good") || incomingMessage.Contains(" i am fine") || 
+                    incomingMessage.Contains(" i am fine") || incomingMessage.Contains("nice") || incomingMessage.Contains("good"))
+                {
+                    messageToSend = "Nice to know that. Shall we get started"; 
+                }
+                else
+                {
+                    if (incomingMessage.Contains("thank you"))
+                        messageToSend = "You are welcome";
+
+                    if (incomingMessage.Contains("goodbye"))
+                        messageToSend = "See you later";
+
+                    if (incomingMessage.Contains("ok") || incomingMessage.Contains("okay") || incomingMessage.Contains("fine") || incomingMessage.Contains("hmm"))
+                        messageToSend = "Want to continue further";
+
+                    if (incomingMessage.Contains("no") || incomingMessage.Contains("nope"))
+                        messageToSend = "Ok, bye hope to see you soon";
+
+
+                }
+
+                this.task.Call(interruption, null);
+                await task.PollAsync(token);
+            }
         }
 
         protected override async Task<string> PrepareAsync(IActivity item, CancellationToken token)
@@ -53,7 +90,9 @@ namespace ProactiveBot.Dialogs.ScorableDialogs
             {
                 var msg = message.Text.ToLowerInvariant();
 
-                if(msg == "hello" || msg == "thank you" || msg == "goodbye")
+                if(msg == "ok" || msg == "thank you" || msg == "goodbye" || msg == "fine" || msg == "hmm" || msg == "good" 
+                    || msg == "nice" || msg == "okay" || msg == "how are you" || msg == "i am fine" || msg == "i am good"
+                    || msg == " i am nice" || msg == "no" || msg == "nope")
                 {
                     return message.Text;
                 }
